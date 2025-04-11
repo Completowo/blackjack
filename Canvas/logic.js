70e4a0c0e4da25254d0b5fda6105921bd917e50f
Vina del Mar <vina@duoc.acad>
2025-04-11 14:32:26 -0400

    Hola

--- Canvas/logic.js
+++ Canvas/logic.js
@@ -5,7 +5,7 @@ const ctx = canvas.getContext("2d")
 const BLOCK_SIZE = 20
 const BOARD_WIDTH = 14
 const BOARD_HEIGHT = 30
-
+let posSnake = [10,3]
 
 canvas.witdh = BLOCK_SIZE * BOARD_WIDTH
 canvas.height = BLOCK_SIZE * BOARD_HEIGHT
@@ -14,8 +14,11 @@ canvas.height = BLOCK_SIZE * BOARD_HEIGHT
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE)
 
 
+
 let mapaArray = crearMapa(BOARD_WIDTH, BOARD_HEIGHT)
-console.log(mapaArray)
+
+
+
 function crearMapa(B_W, B_H) {
     let vmapa = []
 
@@ -27,7 +30,17 @@ function crearMapa(B_W, B_H) {
     }
 
     return vmapa
-} 
+}
+
+function clearMap(){
+    for (let y = 0; mapaArray.length > y; y++){
+        for (let x = 0; mapaArray[y].length > x; x++){
+            mapaArray[y][x] = 0
+        }
+    }
+    return mapaArray
+}  
+
 
 
 function update() {
@@ -39,17 +52,60 @@ function draw(){
     ctx.fillStyle = "000"
     ctx.fillRect(0,0, canvas.witdh, canvas.height)
 
-    mapaArray.forEach((row, y) => {
-        row.forEach((value, x) =>{
+    //Por cada Fila
+    for (let y = 0; mapaArray.length > y; y++){
+        
+        //Por cada columna
+        for (let x; mapaArray[y].length > x; x++){
+
             if (value == 0){
                 ctx.fillStyle = "green"
                 ctx.fillRect(x, y, 1, 1)
-            }
-        })
-    })
-    
 
+            } else if (value == 1){
+                ctx.fillStyle = "red"
+                ctx.fillRect(x, y, 1, 1)
+
+            }
+        }
+    }
 }
+
+function snake(){
+    mapaArray[posSnake[0], posSnake[1]] = 1
+
+    document.addEventListener("keydown", function(keyboard){
+        if(keyboard.key == "ArrowUp"){
+            posSnake = moveSnake(posSnake, 0)
+            mapaArray[posSnake[0], posSnake[1]] = 1
+            console.log(mapaArray)
+        }
+    })
+}
+
+function moveSnake(posAct, direc){
+    switch (direc){
+        //Arriba
+        case 0: 
+            posAct[0] = posAct[0] -1 
+        //Derecha
+        case 1:
+            posAct[1] = posAct[1] +1
+        //abajo
+        case 2:
+            posAct[0] = posAct[0] +1
+        //Izquierda
+        case 3:
+            posAct[1] = posAct[1] -1
+    }
+    return posAct
+}
+
+
+snake()
+
+console.log(mapaArray)
+
 
 
 update();
